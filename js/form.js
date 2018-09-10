24314545c4f9f0ebaa73a9c365ee92219563d5c1
Jon Washington <jonathan.washington@gmail.com>
2018-09-09 17:20:38 -0700

    grabInput function returns length of ingredients remaining

--- js/form.js
+++ js/form.js
@@ -23,6 +23,31 @@ function grabInput(){
     console.log('the loop is iterating for the ' + i + 'th time.');
     if (storedSpirit = vodkaCocktails[i].spirit){
       console.log('the spirit matches and belongs to '+ vodkaCocktails[i].name);
+      var recipeClone = vodkaCocktails[i].rawIngredients.slice(0);
+      for (var number = 0; number < storedIngredients.length; number++){
+        console.log(recipeClone);
+
+        var match = recipeClone.indexOf(storedIngredients[number])
+        console.log(match);
+        if (match >= 0){
+          recipeClone.splice((match), 1);
+        };
+
+        console.log(recipeClone);
+
+
+        // for (var foo = 0; foo < storedIngredients.length; foo++){
+        //   console.log('confusing enough? The current user ingredient is ' + storedIngredients[foo] + ' and the current recipe ingredient is ' + recipeClone[number]);
+        //   if (recipeClone[number] == storedIngredients[foo]){
+        //     break;
+        //   }
+        //   else{
+        //     vodkaCocktails[i].itemsMissing += 1;
+        //
+        //     console.log('Ok! ' +storedIngredients[foo] + ' does not match. Now the items missing for the '+ vodkaCocktails[i].name +  ' cocktail is ' + vodkaCocktails[i].itemsMissing);
+        //   }
+        // }
+      }
     }
   }
 }
--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function Cocktail(spirit, name, ingredients, rawIngredients, image, howToMake) {
     this.rawIngredients= rawIngredients;
     this.image = image;
     this.howToMake = howToMake;
-    this.itemsMissing = [];
+    this.itemsMissing = 100;
 }
 
 var vodkaCocktails = [];
