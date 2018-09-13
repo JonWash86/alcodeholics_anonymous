7b538b2b1a8a45ac02e54201dee58197aa24d58b
Jon Washington <jonathan.washington@gmail.com>
2018-09-12 18:44:29 -0700

    first commit on organization branch. taking break to merge results display function

--- js/form.js
+++ js/form.js
@@ -3,7 +3,7 @@ function storeIngredients(){
   localStorage.setItem('spirit', JSON.stringify(spiritSelection));
   var options = [];
   var ingredients = document.getElementsByTagName('input');
-  for (var i = 0; i < ingredients.length; i++){
+  for (var i =0; i < ingredients.length; i++){
     var ingredient = ingredients[i];
     if (ingredient.type == 'checkbox' && ingredient.checked){
       options[options.length] = ingredient.value;
@@ -22,56 +22,18 @@ function scoreMatch(){
       var recipeClone = allCocktailList[i].rawIngredients.slice();
       console.log('the recipe clone for ' + allCocktailList[i].name + ' is ' + recipeClone);
       for (var number = 0; number < storedIngredients.length; number++){
-        // console.log('the user-submitted ingredient being examined right now is ' + storedIngredients[number]);
         var match = recipeClone.indexOf(storedIngredients[number])
         if (match >= 0){
           console.log('matched index ' + recipeClone.indexOf(storedIngredients[number]));
           recipeClone.splice((match), 1);
         };
-
-        vodkaCocktails[i].score = recipeClone.length;
-      }
-    }
-  }
-
-}
-
-
-//jc code
-//this code should run when user hits submit button
-function listCocktails() {
-  
-  var localSpirit = localStorage.getItem('spirit');   
-  cocktailRecipes[localSpirit].sort(function (a, b) {
-      return a.score - b.score;
-    });
-
-    for(var cocktailIndex = 0; cocktailIndex < allCocktails[i].length; cocktailIndex++) {
-      var eachCocktail = document.createElement('div');
-      eachCocktail.setAttribute('class', 'cocktail');
-      eachCocktail.addEventListener('click', expandPopup);
-      eachCocktail.dataset.drink = allCocktails[i][cocktailIndex].name;
-      var cocktailImage = document.createElement('img');
-      cocktailImage.setAttribute('src', 'images/cocktail_imgs/' + allCocktails[i][cocktailIndex].image);
-      var cocktailTitle = document.createElement('h2');
-      cocktailTitle.innerText = allCocktails[i][cocktailIndex].name;
-      eachCocktail.appendChild(cocktailImage);
-      eachCocktail.appendChild(cocktailTitle);
-      createSpiritDiv.appendChild(eachCocktail);
-  }
-    
-
-    
-  }
-
-
-
-window.addEventListener('load', localIngredients);
-
-
         allCocktailList[i].score = recipeClone.length;
         console.log('the remaining ingredients number at ' + recipeClone.length);
       };
     };
   };
+};
+
+function sayHello(){
+  console.log('Hello!');
 };
